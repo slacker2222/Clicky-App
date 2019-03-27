279a4c688c0d18d3fd4ff89866a4a3802a333c25
Stacy Acker <stacyacker@Stacys-MacBook-Pro.local>
2019-03-26 20:17:39 -0400

    testing heroku issue

---
+++ build/precache-manifest.9fd5ca895fb9b47995b62ef656720d89.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ce7cf31662f932f137d",
+    "url": "/Clicky-App/static/js/runtime~main.fd8b7ae3.js"
+  },
+  {
+    "revision": "abcc45e1d92a741a6a65",
+    "url": "/Clicky-App/static/js/main.99da9e4b.chunk.js"
+  },
+  {
+    "revision": "85f1719d56c8f7d51e2c",
+    "url": "/Clicky-App/static/js/2.278c4d8b.chunk.js"
+  },
+  {
+    "revision": "abcc45e1d92a741a6a65",
+    "url": "/Clicky-App/static/css/main.337fee5d.chunk.css"
+  },
+  {
+    "revision": "83b9b4c3318d5a1e229ad89a479dec5d",
+    "url": "/Clicky-App/index.html"
+  }
+];

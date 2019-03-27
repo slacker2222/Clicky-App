8eb95a8182ce28a4d4fbfc55713b6aa567c82237
Stacy Acker <stacyacker@Stacys-MacBook-Pro.local>
2019-03-26 21:10:56 -0400

    fixed heroku

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 
 const Card = props => (
     <div className="imgCard">
-    <img alt={props.name} src={"https://glacial-oasis-89629.herokuapp.com/" + props.image} id={props.id}  onClick = {() => props.shufflePics(props.id)} className='shufflePics'/>
+    <img alt={props.name} src={process.env.PUBLIC_URL + props.image} id={props.id}  onClick = {() => props.shufflePics(props.id)} className='shufflePics'/>
 
     </div>
 )

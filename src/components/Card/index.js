1eb05bd444c923597d391c0b7bff34d6cb948876
Stacy Acker <stacyacker@Stacys-MacBook-Pro.local>
2019-03-26 20:29:33 -0400

    added Key to heroku

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 
 const Card = props => (
     <div className="imgCard">
-    {/* <img alt={props.name} src={process.env.PUBLIC_URL + props.images} id={props.id}  onClick = {() => props.shufflePics(props.id)} className='shufflePics'/>/> */}
+    <img alt={props.name} src={process.env.PUBLIC_URL + props.images} id={props.id}  onClick = {() => props.shufflePics(props.id)} className='shufflePics'/>/>
 
     </div>
 )
